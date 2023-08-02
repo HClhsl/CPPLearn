f9debbf844609e40bacd8617341bd49e898fc5b7
HClhsl <2858032170@qq.com>
2023-08-02 13:42:10 +0800

    change

--- 8.1.3.cpp
+++ 8.1.3.cpp
@@ -1,9 +1,36 @@
 // Created on iPad.
 
-#include <iostream>
-using namespace std;
+#include <cstdio>
+#include <cstdlib>
+#define MAX_LENGTH 50
+
+typedef int ElementType;
+
+typedef struct LNode{
+    ElementType data;
+    LNode *next;
+}LNode,*linkList;
 
 int main() {
-    cout << "Hello World!";
+    ElementType x;
+    int n=3;
+    linkList p,q;
+    p=(LNode*)malloc(sizeof(LNode));//头节点空间申请
+    p->next=(LNode*) malloc(sizeof (LNode));
+    q=p->next;
+    scanf("%d",&x);
+    q->data=x;
+    while(n){
+        q->next=(LNode*) malloc(sizeof (LNode));
+        q=q->next;
+        scanf("%d",&x);
+        q->data=x;
+        q->next=NULL;
+        n--;
+    }
+    //q->next=NULL;
+    for(linkList k=p->next;k!=NULL;k=k->next){
+        printf("%d\t",k->data);
+    }
     return 0;
 }
